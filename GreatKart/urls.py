bccf2288cf51fc8bac6290ad35bee76ad6c2c448
legendajar <harshsolanki9898@gmail.com>
2024-03-09 23:33:47 +0530

    Add product detail view and update URLs

--- GreatKart/urls.py
+++ GreatKart/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('store/', include('store.urls'))
+    path('', views.home, name="home"),
+    path('store/', include('store.urls'), name="store"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
