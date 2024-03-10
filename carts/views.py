39b551e40e20ecca26e7724ba0a2fddf0b601229
legendajar <harshsolanki9898@gmail.com>
2024-03-11 02:17:18 +0530

    Refactor saving of cart item in add_cart function

--- carts/views.py
+++ carts/views.py
@@ -64,7 +64,7 @@ def add_cart(request, product_id):
             if len(product_variation) > 0:
                 item.variations.clear()
                 item.variations.add(*product_variation)
-            cart_item.save()
+            item.save()
 
     else:
         cart_item = CartItem.objects.create( product = product, quantity = 1, cart = cart)
