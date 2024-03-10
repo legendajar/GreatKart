4cd201c067f56839a19723c21d9e1896842768d0
legendajar <harshsolanki9898@gmail.com>
2024-03-10 19:08:17 +0530

    Update store URLs and add search functionality

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
-
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def _cart_id(request):
@@ -56,6 +55,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -65,7 +66,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         tax = (2 * total)/100
         grand_total = total + tax
 
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass
 
     context = {
