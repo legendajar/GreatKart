81ca0af3fe9c2173ffe0af37a5e710e2a38834ed
legendajar <harshsolanki9898@gmail.com>
2024-03-11 17:01:33 +0530

    Update settings and urls files, add registration, login, and logout views

--- GreatKart/settings.py
+++ GreatKart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# Django Messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
