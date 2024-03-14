e8c8eb51eede646c12bfc9d1a9bcba14f738f986
legendajar <harshsolanki9898@gmail.com>
2024-03-14 13:31:25 +0530

    Remove email credentials from settings.py

--- GreatKart/settings.py
+++ GreatKart/settings.py
@@ -153,6 +153,5 @@ MESSAGE_TAGS = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'harshsolanki9898@gmail.com'
-EMAIL_HOST_PASSWORD = 'girt fviq bqrs wwuq'
+
 EMAIL_USE_TLS = True
